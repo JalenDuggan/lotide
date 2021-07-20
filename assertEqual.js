5205897a0a57d8731b42c2b40b421a37f4f65f93
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-07-20 12:07:45 +0000

    Added a assertEqual function that compares input

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  const check = String.fromCodePoint(0x2705);
+  const x = String.fromCodePoint(0x274c);
+  if (actual === expected) {
+    console.log(check + check + check + " Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log(x + x + x + " Assertion Failed: [actual] !== [expected] ");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);

4078f28526055ed0cacf6e5d28e36f941952c6c0
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-07-21 17:05:24 +0000

    Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function(actual, expected) {
+  const check = String.fromCodePoint(0x2705);
+  const x = String.fromCodePoint(0x274c);
+  if (actual.length !== expected.length) {
+    return console.log(`${x} ${x} ${x} Assertion Failed: [actual] !== [expected] `);
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return console.log(`${x} ${x} ${x} Assertion Failed: [actual] !== [expected] `);
+    }
+    
+  }
+  return console.log(`${check} ${check} ${check} Assertion Passed: [actual] === [expected]`);
+
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3], true); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3], true); // => should Fail
+
+

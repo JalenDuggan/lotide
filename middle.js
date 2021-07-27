8f8e06dbd90423d658bb5987f7203a2d71058ae2
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-07-27 11:27:47 +0000

    Cleaned up code using modules and require

--- middle.js
+++ middle.js
@@ -1,31 +1,6 @@
-const eqArrays = function (arry1, arry2) {
-  if (arry1.length !== arry2.length) {
-    return false;
-  }
-  for (let i = 0; i < arry1.length; i++) {
-    if (arry1[i] !== arry2[i]) {
-      return false;
-    }
-    
-  }
-  return true;
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(actual, expected) {
-  const check = String.fromCodePoint(0x2705);
-  const x = String.fromCodePoint(0x274c);
-  if (actual.length !== expected.length) {
-    return console.log(`${x} ${x} ${x} Assertion Failed: [actual] !== [expected] `);
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return console.log(`${x} ${x} ${x} Assertion Failed: [actual] !== [expected] `);
-    }
-    
-  }
-  return console.log(`${check} ${check} ${check} Assertion Passed: [actual] === [expected]`);
 
-};
 
 
 const middle = function (array) {
@@ -44,8 +19,6 @@ const middle = function (array) {
   }
 }
 
+module.exports = middle;
 
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
 
-assertArraysEqual((middle([1,2,3])), [2]);

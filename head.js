3a145f7c8e1d65fc6c43b7fb4947c222b3ab1b8c
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-07-27 09:06:23 +0000

    Removed commited function

--- head.js
+++ head.js
@@ -1,13 +1,3 @@
-// const assertEqual = function(actual, expected) {
-//   const check = String.fromCodePoint(0x2705);
-//   const x = String.fromCodePoint(0x274c);
-//   if (actual === expected) {
-//     return console.log(`${check} ${check} ${check} Assertion Passed: [actual] === [expected]`);
-//   }
-//   if (actual !== expected) {
-//     return console.log(`${x} ${x} ${x} Assertion Failed: [actual] !== [expected] `);
-//   }
-// };
 
 const assertEqual = require('./assertEqual');
 
